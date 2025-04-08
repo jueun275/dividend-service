0bc3e24870f90ab7ba58249252fa02861d5294c9
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-04-08 17:07:08 +0900

    Initial commit

---
+++ src/test/java/com/example/dividend/DividendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dividend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
