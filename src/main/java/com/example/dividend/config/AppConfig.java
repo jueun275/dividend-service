c472f8574caa3605336619235afef4c7ddafb68d
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-04-09 02:41:58 +0900

    refector: AppConfig 패키지 이동

--- src/main/java/com/example/dividend/config/AppConfig.java
+++ src/main/java/com/example/dividend/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.example.dividend.model.constants;
+package com.example.dividend.config;
 
 import org.apache.commons.collections4.Trie;
 import org.apache.commons.collections4.trie.PatriciaTrie;
